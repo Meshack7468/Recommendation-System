b57f9c405be4ed5af15e06eb767061cc3f718dce
Meshack7468 <meshack.mboya@student.moringaschool.com>
2026-05-30 00:16:51 +0300

    modified recommender.py file

--- Core/recommender.py
+++ Core/recommender.py
@@ -1,55 +1,37 @@
-import os
 import pickle
-import pandas as pd
-import gdown
 
-# Google Drive file IDs
-MOVIES_FILE_ID = "1pmBMgtLnU8jNQci75aRmUX95SvPcC2Q4"
-SIMILARITY_FILE_ID = "1TvpvjIfNkZM9g_D7CdItlZCcC3IVkyBf"
+# Load data (stored locally in Core/)
+movies = pickle.load(open("movies.pkl", "rb"))
+similarity = pickle.load(open("similarity.pkl", "rb"))
 
-
-# Download helper
-def download_file(file_id, output_name):
-    if not os.path.exists(output_name):
-        url = f"https://drive.google.com/uc?id={file_id}&export=download"
-        gdown.download(url, output_name, quiet=False)
-
-# Download models
-download_file(MOVIES_FILE_ID, "movies.pkl")
-download_file(SIMILARITY_FILE_ID, "similarity.pkl")
-
-# Load data
-movies = pickle.load(open('movies.pkl', 'rb'))
-similarity = pickle.load(open('similarity.pkl', 'rb'))
 
 # Return all movie titles
 def get_movie_titles():
-    return movies['title'].tolist()
+    return movies["title"].tolist()
+
 
 # Recommendation function
 def recommend_movies(movie):
     # Check if movie exists
-    if movie not in movies['title'].values:
+    if movie not in movies["title"].values:
         return []
-    
+
     # Fetch movie index
-    movie_index = movies[movies['title'] == movie].index[0]
-    
+    movie_index = movies[movies["title"] == movie].index[0]
+
     # Get similarity scores
     distances = similarity[movie_index]
-    
-    # Sort movies based on similarity
+
+    # Sort movies based on similarity score
     movies_list = sorted(
         list(enumerate(distances)),
         reverse=True,
         key=lambda x: x[1]
     )[1:10]
-    
-    recommended_movies = []
-    for i in movies_list:
-        recommended_movies.append(
-            movies.iloc[i[0]].title
-        )
-    
-    
+
+    # Build recommendations
+    recommended_movies = [
+        movies.iloc[i[0]].title for i in movies_list
+    ]
+
     return recommended_movies
